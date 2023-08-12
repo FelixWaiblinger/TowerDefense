cf1b16a5bfb27cfe8743eb4be421775cff4e1cbc
Felix Waiblinger <diebreze@gmail.com>
2023-08-13 00:11:37 +0200

    updated FPS

--- Assets/Scripts/Managers/FPSManager.cs
+++ Assets/Scripts/Managers/FPSManager.cs
@@ -1,11 +1,21 @@
 using UnityEngine;
+using TMPro;
 
 public class FPSHandler : MonoBehaviour
 {
     [SerializeField] private int _targetFPS = 60;
+    [SerializeField] private bool _showFPS = false;
+    [SerializeField] private GameObject _framesUI;
+    [SerializeField] private TMP_Text _FPS;
 
     void Start()
     {
         Application.targetFrameRate = _targetFPS;
+        if (_showFPS) _framesUI.SetActive(true);
+    }
+
+    void FixedUpdate()
+    {
+        _FPS.text = $"{(int)(1 / Time.deltaTime)} FPS";
     }
 }
