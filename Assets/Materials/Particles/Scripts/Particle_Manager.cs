118171a97df6ec741cfb0e6c10626be1dbcaf762
Patrick <paddyblues@gmx.de>
2023-08-15 02:46:33 +0200

    Testing before it crashes

--- Assets/Materials/Particles/Scripts/Particle_Manager.cs
+++ Assets/Materials/Particles/Scripts/Particle_Manager.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Particle_Manager : MonoBehaviour
-{
+public class Particle_Manager : MonoBehaviour {
     public float _speed = 15f;
     public Vector3 _rotationOffset = new Vector3(0, 0, 0);
 
     private Rigidbody _rb;
 
     public GameObject[] Detachables;
+
+    [Tooltip("Only for testing Purposes")]
+    public GameObject selfPrefab;
 
     private void Start() {
         _rb = GetComponent<Rigidbody>();
@@ -21,5 +23,20 @@ public class Particle_Manager : MonoBehaviour
 
         }
     }
+    private void OnCollisionEnter(Collision collision) {
+        _rb.constraints = RigidbodyConstraints.FreezeAll;
+        _speed = 0;
 
+        //Removing trail from the projectile on cillision enter or smooth removing. Detached elements must have "AutoDestroying script"
+        foreach (var detachedPrefab in Detachables) {
+            if (detachedPrefab != null) {
+                detachedPrefab.transform.parent = null;
+                Destroy(detachedPrefab, 1);
+            }
+        }
+        //Destroy projectile on collision
+        Destroy(gameObject);
+
+
+    }
 }
